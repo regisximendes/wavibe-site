dd20a78c0e2c51c6b81a9896febb9e493241f4cc
Regis Ximendes <regisximendes.dev@gmail.com>
2023-10-15 21:30:27 -0300

    remover mongo uri

--- config/config.js
+++
@@ -1,8 +0,0 @@
-global.SALT_KEY = "abc";
-
-module.exports = {
-
-    connectionString : 'mongodb://admin:admin123@ds127968.mlab.com:27968/form',
-    sendegridKey:'TBD',
-    containerConnectionString:'TBD'
-}
--- config/server.js
+++ config/server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
 const http = require('http');
-const config = require('./config.js');
 const app = express();
 
 app.set('view engine', 'ejs')
